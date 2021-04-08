c2ca832713eb47f2cfabc334222eaf0e6720ab0f
JoshiRaez <joseraezrodriguez@gmail.com>
2021-04-08 18:16:49 +0200

    Added java test

---
+++ test/languages/java_test.rb
@@ -0,0 +1,67 @@
+require "test_helper"
+
+module SnippetExtractor
+  module Languages
+    class JavaTest < Minitest::Test
+      def test_full_example
+        code = <<~CODE
+          package example;
+
+          /**
+          * Multiline comment
+          */
+
+          //Inline comment
+
+          /* Block comment */
+
+          import java.io.BufferedReader;
+          import java.io.IOException;
+          import java.io.InputStreamReader;
+          import java.util.*;
+          import java.util.stream.Collectors;
+
+          public class Solution {
+            public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            public static void main(String[] args) throws Exception {
+
+              var cases = readInt();
+
+              for (int caseN = 0; caseN < cases; caseN++) {
+                var data = readInt();
+                System.out.println(data);
+              }
+            }
+
+            private static int readInt() throws IOException {
+              return Integer.parseInt(in.readLine());
+            }
+          }
+
+          CODE
+
+        expected = <<~CODE
+          public class Solution {
+            public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            public static void main(String[] args) throws Exception {
+
+              var cases = readInt();
+
+              for (int caseN = 0; caseN < cases; caseN++) {
+                var data = readInt();
+                System.out.println(data);
+              }
+            }
+
+            private static int readInt() throws IOException {
+              return Integer.parseInt(in.readLine());
+            }
+          }
+
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :php)
+      end
+    end
+  end
+end
