a58c4ebadffc88a15221d596139ee0f47f9a642b
BethanyG <BethanyG@users.noreply.github.com>
2021-04-08 04:40:17 -0700

    Added Python (#4)
    * Added python to lib/languages and test/languages.
    
    * Updated test for python.
    
    * Additonal adjustments for tests.
    
    * Another try, with spelling errors corrected.
    
    * One more try with changed indentation for test case.
    
    * And one more time, with feeling.
    
    * Added import and from to skip list and adjusted test case.

---
+++ test/languages/python_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+module SnippetExtractor
+  module Languages
+    class PythonTest < Minitest::Test
+      def test_full_example
+        code = <<~CODE
+          #!/usr/bin/env python3
+          # The above is sometimes used to note
+          # where the python interpreter is located.
+
+          # And after this comes the import statements, if any:
+          import re
+          from collections import Counter
+
+          # And then any constants or globals,
+          # followed by code.
+          WORDS = re.compile("[a-z0-9]+(['][a-z]+)?")
+
+          def count_words(text):
+              return Counter(word.group(0) for word in WORDS.finditer(text.lower()))
+        CODE
+
+        expected = <<~CODE
+          WORDS = re.compile("[a-z0-9]+(['][a-z]+)?")
+
+          def count_words(text):
+              return Counter(word.group(0) for word in WORDS.finditer(text.lower()))
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :python)
+      end
+    end
+  end
+end
