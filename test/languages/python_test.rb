1c2a8fa15050c1d81b5a7797f2209ec106dab52d
BethanyG <bethanymgarcia@gmail.com>
2021-04-07 17:05:21 -0700

    And one more time, with feeling.

--- test/languages/python_test.rb
+++ test/languages/python_test.rb
@@ -5,30 +5,30 @@ module SnippetExtractor
     class PythonTest < Minitest::Test
       def test_full_example
         code = <<~CODE
-        #!/usr/bin/env python3
-        # The above is sometimes used to note
-        # where the python interpreter is located
+          #!/usr/bin/env python3
+          # The above is sometimes used to note
+          # where the python interpreter is located
 
-        # And after this comes the import statements, if any:
-        import re
-        from collections import Counter
+          # And after this comes the import statements, if any:
+          import re
+          from collections import Counter
 
-        # And then any constants or globals
-        WORDS = re.compile("[a-z0-9]+(['][a-z]+)?")
+          # And then any constants or globals
+          WORDS = re.compile("[a-z0-9]+(['][a-z]+)?")
 
-        # Finally, some code:
-        def count_words(text):
-            return Counter(word.group(0) for word in WORDS.finditer(text.lower()))
+          # Finally, some code:
+          def count_words(text):
+              return Counter(word.group(0) for word in WORDS.finditer(text.lower()))
         CODE
 
         expected = <<~CODE
-        import re
-        from collections import Counter
+          import re
+          from collections import Counter
 
-        WORDS = re.compile("[a-z0-9]+(['][a-z]+)?")
+          WORDS = re.compile("[a-z0-9]+(['][a-z]+)?")
 
-        def count_words(text):
-            return Counter(word.group(0) for word in WORDS.finditer(text.lower()))
+          def count_words(text):
+              return Counter(word.group(0) for word in WORDS.finditer(text.lower()))
         CODE
 
         assert_equal expected, ExtractSnippet.(code, :python)
