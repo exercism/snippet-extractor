553f4a2e7586c2d96164e3efa2f56d48d3c10dcf
José Ráez Rodríguez <joseraezrodriguez@gmail.com>
2021-04-08 19:03:31 +0200

    Typescript added (#16)
    * Typescript added
    
    * Change class name of typescript test
    
    * Fixed typescript test
    
    * Fixing identation in typescript test

---
+++ test/languages/typescript_test.rb
@@ -0,0 +1,49 @@
+require "test_helper"
+
+module SnippetExtractor
+  module Languages
+    class TypescriptTest < Minitest::Test
+      def test_full_example
+        code = <<~CODE
+
+          // Skipping imports
+          import * as example from 'example.js'
+          import *
+            as example
+            from 'example.js'
+
+          import { get_data, get_data2 } from 'example.js
+
+          /// Saying things over
+          /// multiple lines of code
+
+          /* Multiline comments */
+          /**
+           * Multiline comments 2
+           *
+           */
+
+          let numberRegexp = /^[0-9]+$/;
+          class ZipCodeValidator {
+            isAcceptable(s: string) {
+              return s.length === 5 && numberRegexp.test(s);
+            }
+          }
+          export = ZipCodeValidator;
+        CODE
+
+        expected = <<~CODE
+          let numberRegexp = /^[0-9]+$/;
+          class ZipCodeValidator {
+            isAcceptable(s: string) {
+              return s.length === 5 && numberRegexp.test(s);
+            }
+          }
+          export = ZipCodeValidator;
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :typescript)
+      end
+    end
+  end
+end
