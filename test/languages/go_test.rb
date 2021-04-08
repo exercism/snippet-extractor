c40168c83658e506d60547a8259fb1ca9b450be3
André Santos <andrerfcsantos@gmail.com>
2021-04-08 01:22:55 +0100

    Add Go (#5)

---
+++ test/languages/go_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+module SnippetExtractor
+  module Languages
+    class GoTest < Minitest::Test
+      def test_full_example
+        code = <<~CODE
+          // Package gigasecond constains functionality to add a gigasecond
+          // to a period of time
+          package gigasecond
+
+          import "time"
+
+          // AddGigasecond adds a Gigasecond to the given time
+          func AddGigasecond(t time.Time) time.Time {
+            return t.Add(time.Second * 1e9)
+          }
+        CODE
+
+        expected = <<~CODE
+          func AddGigasecond(t time.Time) time.Time {
+            return t.Add(time.Second * 1e9)
+          }
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :go)
+      end
+    end
+  end
+end
