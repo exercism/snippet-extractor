d0ba403bc11aea8a558326864fe25cdbc4cd00eb
BethanyG <bethanymgarcia@gmail.com>
2021-04-07 18:14:41 -0700

    Added import and from to skip list and adjusted test case.

--- test/languages/python_test.rb
+++ test/languages/python_test.rb
@@ -7,24 +7,21 @@ module SnippetExtractor
         code = <<~CODE
           #!/usr/bin/env python3
           # The above is sometimes used to note
-          # where the python interpreter is located
+          # where the python interpreter is located.
 
           # And after this comes the import statements, if any:
           import re
           from collections import Counter
 
-          # And then any constants or globals
+          # And then any constants or globals,
+          # followed by code.
           WORDS = re.compile("[a-z0-9]+(['][a-z]+)?")
 
-          # Finally, some code:
           def count_words(text):
               return Counter(word.group(0) for word in WORDS.finditer(text.lower()))
         CODE
 
         expected = <<~CODE
-          import re
-          from collections import Counter
-
           WORDS = re.compile("[a-z0-9]+(['][a-z]+)?")
 
           def count_words(text):
