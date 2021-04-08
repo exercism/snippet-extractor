59d36fc920422ea21d9cdf3d6f6d303302a5f513
ynfle <23086821+ynfle@users.noreply.github.com>
2021-04-08 18:08:14 +0300

    [Nim] add nim extractor (#13)
    * [Nim] Add nim to languages
    
    * [Nim] Test: add test for nim
    
    * [Nim] Fix: include `include`
    
    Co-authored-by: ee7 <45465154+ee7@users.noreply.github.com>
    
    * [Nim] Fix: fix mistakes from copying from ruby
    
    Co-authored-by: ee7 <45465154+ee7@users.noreply.github.com>
    
    * [Nim] Fix: remove multiline comment closing tag
    
    Co-authored-by: ee7 <45465154+ee7@users.noreply.github.com>
    
    * [Nim] Fix: Include all lines after the first not ignored line
    
    Co-authored-by: ee7 <45465154+ee7@users.noreply.github.com>
    
    * [Nim] Feature: add regular comment at the end
    
    * [Nim] Feature: add doc comment to be skipped
    
    Co-authored-by: ee7 <45465154+ee7@users.noreply.github.com>

---
+++ test/languages/nim_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+
+module SnippetExtractor
+  module Languages
+    class NimTest < Minitest::Test
+      def test_full_example
+        code = <<~CODE
+          ## Module doc comment
+          # This is a file
+          # With some comments in it
+
+          # And a blank line ⬆️
+          # It has some module-related code like this:
+          import json
+          export json
+          from json import nil
+          # And then eventually the code
+          proc twoFer =
+            ##[ doc comment ]##
+            ## more doc comment
+            discard
+          # A non doc comment
+        CODE
+
+        expected = <<~CODE
+          proc twoFer =
+            ##[ doc comment ]##
+            ## more doc comment
+            discard
+          # A non doc comment
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :nim)
+      end
+    end
+  end
+end
