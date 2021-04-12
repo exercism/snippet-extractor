068746eab9e54ef3e2156d5af1f8fe62f724123f
joshiraez <joseraezrodriguez@gmail.com>
2021-04-12 03:56:45 +0200

    Rule parser should bring simple rule with the given modifiers

--- test/snippet_extractor_extended/rule_parser/rule_parser_test.rb
+++ test/snippet_extractor_extended/rule_parser/rule_parser_test.rb
@@ -53,5 +53,19 @@ module SnippetExtractorExtended
       # Then
       assert_equal [SimpleRule.new('word','j')], rule_list
     end
+
+    def test_just_partial_word_creates_simple_rule_with_just_and_partial_modifiers
+      # Given
+      rule_text =
+        %q{word\pj
+        }
+
+      # When
+      rule_list = RuleParser.(rule_text)
+
+      # Then
+      assert_equal [SimpleRule.new('word','pj')], rule_list
+    end
+
   end
 end
