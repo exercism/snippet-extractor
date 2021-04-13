957c6f55dec6b1cf77597c054497982983a21b0c
Jeremy Walker <jez.walker@gmail.com>
2021-04-13 01:30:29 +0100

    Fix up tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,7 +17,7 @@ class Minitest::Test
 end
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-Dir[File.join(".", "**/*.rb")].sort.each { |f| require f unless f[%r{^\./test/}] }
+require "snippet_extractor"
 
 class Minitest::Test
 end
