9c2789251956c0ca66db2885921daafe77400e6e
joshiraez <joseraezrodriguez@gmail.com>
2021-04-14 04:18:53 +0200

    Syntax trie saves whole word multiple rules

--- test/extended/syntax_trie_factory_test.rb
+++ test/extended/syntax_trie_factory_test.rb
@@ -41,6 +41,50 @@ module SnippetExtractor
         assert_equal expected, syntax_trie
       end
 
+      def test_two_words_without_same_substring
+        # Given
+        rules = [SimpleRule.new('word',''), SimpleRule.new('asd','')]
+        expected = syntax_trie_maker(
+          {
+            ' ':{
+               'w':{'o':{'r':{'d':{' ': [{}, SimpleRule.new('word','')]}}}},
+               'a':{'s':{'d':{' ':[{},SimpleRule.new('asd','')]}}}
+             }})
+
+        # When
+        syntax_trie = SyntaxTrieFactory.(rules)
+
+        # Then
+        assert_equal expected, syntax_trie
+      end
+
+      def test_two_words_with_same_root
+        # Given
+        rules = [SimpleRule.new('word',''), SimpleRule.new('w','j')]
+        expected = syntax_trie_maker(
+          {
+            ' ':{
+              'w':{
+                'o':{'r':{'d':{' ': [{}, SimpleRule.new('word','')]}}},
+                ' ':[{},  SimpleRule.new('w','j')]
+              },
+            }})
+
+        # When
+        syntax_trie = SyntaxTrieFactory.(rules)
+
+        # Then
+        assert_equal expected, syntax_trie
+      end
+
+      def test_two_conflicting_rules
+        # Given
+        rules = [SimpleRule.new('word',''), SimpleRule.new('word','j')]
+
+        # Expect
+        assert_raises { SyntaxTrieFactory.(rules) }
+      end
+
       def syntax_trie_maker(expected_hashes)
         SyntaxTrie.new(syntax_node_maker(expected_hashes, ""))
       end
