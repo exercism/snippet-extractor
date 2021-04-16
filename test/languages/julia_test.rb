d4ac11e6657ee8921f9a5c33234c02b1fcac4b69
Sascha Mann <git@mail.saschamann.eu>
2021-04-17 00:19:27 +0200

    Fix alignment

--- test/languages/julia_test.rb
+++ test/languages/julia_test.rb
@@ -86,20 +86,20 @@ module SnippetExtractor
           Return the total working time in minutes.
           """
           total_working_time(layers, current_time) = preptime(layers) + current_time
-          CODE
+        CODE
 
         expected = <<~CODE
-        """
-            preptime(layers)
-        
-        Return the preparation time in minutes.
-        """
-        preptime(layers) = 2 * layers
-        """
-            remaining_time(current_time)
-        
-        Return the remaining oven time in minutes.
-        """
+          """
+              preptime(layers)
+          
+          Return the preparation time in minutes.
+          """
+          preptime(layers) = 2 * layers
+          """
+              remaining_time(current_time)
+          
+          Return the remaining oven time in minutes.
+          """
         CODE
 
         assert_equal expected, ExtractSnippet.(code, :julia)
