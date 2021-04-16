02cd39aaa14f480368115710214d837d135f6377
JoshiRaez <joseraezrodriguez@gmail.com>
2021-04-17 00:27:03 +0200

    WIP: Code Parser

--- lib/snippet_extractor/extended/code_parser.rb
+++ lib/snippet_extractor/extended/code_parser.rb
@@ -6,8 +6,81 @@ module SnippetExtractor
       initialize_with :code, :syntax_trie
 
       def call
-        return code
+        parse code, syntax_trie
       end
+
+      def parse(code, syntax_trie)
+        parser = initialize_parser code, syntax_trie
+
+        while is_finished? parser
+
+          character = scan parser
+          normalized_character = clean character
+
+          if matches_syntax? parser, normalized_character
+            store_possible_skip parser, character
+            advance_syntax parser, normalized_character
+            next
+          end
+
+          # Rule
+          store_skipped_in_read_line parser
+          reset_syntax parser
+          if matches_syntax? parser, normalized_character
+            store_possible_skip parser, character
+            advance_syntax parser, normalized_character
+          end
+        end
+      end
+
+      def initialize_parser(code, syntax_trie)
+        parser_data = ParserData.new(code, syntax_trie)
+        parser_state = ParserState.new(0, syntax_trie.root, "", "", false)
+        parser_output = ParserOutput.new([])
+
+        Parser.new(parser_data, parser_state, parser_output)
+      end
+
+      def is_finished?(parser)
+        parser.state.scan_index >= parser.data.code.length
+      end
+
+      def scan(parser)
+        read_char = parser.data.code[parser.state.scan_index]
+        parser.scan_index += 1
+
+        read_char
+      end
+
+      def normalize(character)
+        if character.lstrip.empty? then ' ' else character end
+      end
+
+      def matches_syntax?(parser, character)
+        parser.state.current_syntax_trie_node.key? character
+      end
+
+      def store_possible_skip(parser, character)
+        parser.state.skipping_line += character
+      end
+
+      def store_skipped_in_read_line(parser)
+        parser.current_line += parser.skipping_line
+      end
+
+      def advance_syntax(parser, character)
+        parser.state.current_syntax_trie_node[character]
+      end
+
+      def reset_syntax(parser)
+        parser.state.current_syntax_trie_node = parser.data.syntax_trie.root
+      end
+
+      Parser = Struct.new(:data, :state, :output)
+      ParserData = Struct.new(:code, :syntax_trie)
+      ParserState = Struct.new(:scan_index, :current_syntax_trie_node, :current_line, :skipping_line, :multiline_skip?)
+      ParserOutput = Struct.new(:read_lines)
     end
+
   end
 end
--- test/extended/code_parser_test.rb
+++ test/extended/code_parser_test.rb
@@ -19,6 +19,57 @@ module SnippetExtractor
         # Expect
         assert_equal expected, CodeParser.(code, syntax_trie)
       end
+
+      def test_parsing_no_code_returns_no_code
+        # Given
+        syntax_trie = SyntaxTrie.new(
+          Node.new(
+            {' ': Node.new(
+              {'a': Node.new(
+                {'b': Node.new(
+                  {'1': Node.new(
+                    {' ': Node.new({}, ' ab1 ', Line.new('ab1'))}.transform_keys!(&:to_s)
+                  )}.transform_keys!(&:to_s)
+                )}.transform_keys!(&:to_s)
+              )}.transform_keys!(&:to_s)
+            )}.transform_keys!(&:to_s)
+          ))
+        code = ""
+        expected = ""
+
+        # Expect
+        assert_equal expected, CodeParser.(code, syntax_trie)
+      end
+
+      def test_line_rule_whole_word_matches
+        # Given
+        syntax_trie = SyntaxTrie.new(
+          Node.new(
+            {' ': Node.new(
+              {'a': Node.new(
+                {'b': Node.new(
+                  {'1': Node.new(
+                    {' ': Node.new({}, ' ab1 ', Line.new('ab1'))}.transform_keys!(&:to_s)
+                  )}.transform_keys!(&:to_s)
+                )}.transform_keys!(&:to_s)
+              )}.transform_keys!(&:to_s)
+            )}.transform_keys!(&:to_s)
+          ))
+        code =
+          <<~CODE
+            ab1 ad1 ae1
+            ac2 at2 an2
+            cd3 cd3 sdf3
+          CODE
+        expected =
+          <<~CODE
+            ac2 at2 an2
+            cd3 cd3 sdf3
+          CODE
+
+        # Expect
+        assert_equal expected, CodeParser.(code, syntax_trie)
+      end
     end
   end
 end
