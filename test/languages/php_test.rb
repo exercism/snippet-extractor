02a9ed1b4aed3abcb5aeacf537a4e4d1165078cc
joshiraez <joseraezrodriguez@gmail.com>
2021-04-26 00:20:21 +0200

    Php migrated to extended

--- test/languages/php_test.rb
+++ test/languages/php_test.rb
@@ -46,7 +46,59 @@ module SnippetExtractor
                   }
                   return pow(2, $n - 1);
               }
+              function total()
+        CODE
 
+        assert_equal expected, ExtractSnippet.(code, :php)
+      end
+
+      def test_extended_example
+        code = <<~CODE
+          <?php
+
+          namespace Lib\\Exercism\\Test;
+
+          use Lib\\NotExercism;
+
+          // I love C#!! But get paid with PHP
+
+          /*
+           * Comment
+           */
+
+          public class Grains
+          {//comments
+            //Comments
+            /* More 
+              comments*/
+              function square($n/*Even more comments*/)
+              {
+                  if ($n < 1 || $n > 64) {
+                      throw new InvalidArgumentException();//Too large
+                  }
+                  return pow(2, $n - 1);// Just right
+              }
+
+              function total(/*I can be here supposedly*/)
+              {
+                  return array_reduce(range(1, 64), function ($acc, $n) {
+                      return $acc += square($n);
+                  });
+              }//This is the end of the program
+          }
+        CODE
+
+        expected = <<~CODE
+          public class Grains
+          {
+              function square($n)
+              {
+                  if ($n < 1 || $n > 64) {
+                      throw new InvalidArgumentException();
+                  }
+                  return pow(2, $n - 1);
+              }
+              function total()
         CODE
 
         assert_equal expected, ExtractSnippet.(code, :php)
