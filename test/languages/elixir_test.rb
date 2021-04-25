9042131286fd35b326e81f3f6509056da53d1b5f
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-04-25 20:31:36 +0200

    Add more options for sigils

--- test/languages/elixir_test.rb
+++ test/languages/elixir_test.rb
@@ -263,7 +263,7 @@ module SnippetExtractor
         assert_equal expected, ExtractSnippet.(code, :elixir)
       end
 
-      def test_single_line_docs
+      def test_documentation_single_line_string
         # Code snippet taken from https://culttt.com/2016/10/19/writing-comments-documentation-elixir/
         code = <<~CODE
           defmodule Maths do
@@ -273,6 +273,64 @@ module SnippetExtractor
             @type t :: any()
 
             @doc "Add two numbers together"
+            def add(left, right) do
+              # Add two numbers together
+              left + right
+            end
+          end
+        CODE
+
+        expected = <<~CODE
+          defmodule Maths do
+            @type t :: any()
+            def add(left, right) do
+              left + right
+            end
+          end
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :elixir)
+      end
+
+      def test_documentation_single_line_string_sigil_downcase_various_delimiters
+        # Code snippet taken from https://culttt.com/2016/10/19/writing-comments-documentation-elixir/
+        code = <<~CODE
+          defmodule Maths do
+            @moduledoc ~s[A module that implements functions for performing simple mathematic calculations]
+
+            @typedoc ~s(anything...)
+            @type t :: any()
+
+            @doc ~s<Add two numbers together>
+            def add(left, right) do
+              # Add two numbers together
+              left + right
+            end
+          end
+        CODE
+
+        expected = <<~CODE
+          defmodule Maths do
+            @type t :: any()
+            def add(left, right) do
+              left + right
+            end
+          end
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :elixir)
+      end
+
+      def test_documentation_single_line_string_sigil_upcase_various_delimiters
+        # Code snippet taken from https://culttt.com/2016/10/19/writing-comments-documentation-elixir/
+        code = <<~CODE
+          defmodule Maths do
+            @moduledoc ~S/A module that implements functions for performing simple mathematic calculations/
+
+            @typedoc ~S|anything...|
+            @type t :: any()
+
+            @doc ~S{Add two numbers together}
             def add(left, right) do
               # Add two numbers together
               left + right
