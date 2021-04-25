445931964ee62a375ad6458146c95a4f466bae16
JoshiRaez <joseraezrodriguez@gmail.com>
2021-04-25 18:14:06 +0200

    Migrated JS to extended

--- test/languages/javascript_test.rb
+++ test/languages/javascript_test.rb
@@ -12,7 +12,7 @@ module SnippetExtractor
             as example
             from 'example.js'
 
-          import { get_data, get_data2 } from 'example.js
+          import { get_data, get_data2 } from 'example.js'
 
           /// Saying things over
           /// multiple lines of code
@@ -31,6 +31,52 @@ module SnippetExtractor
             xhr.open('GET', url, true);
             xhr.send();
           }
+        CODE
+
+        expected = <<~CODE
+          function getJSON(url, callback) {
+            let xhr = new XMLHttpRequest();
+            xhr.onload = function () {
+              callback(this.responseText)
+            };
+            xhr.open('GET', url, true);
+            xhr.send();
+          }
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :javascript)
+      end
+
+      def test_extended_example
+        code = <<~CODE
+
+          // Skipping imports
+          import * as example from 'example.js'
+          import *
+            as example
+            from 'example.js'
+
+          import { get_data, get_data2 } from 'example.js'
+
+          function getJSON(url, callback) {
+            let xhr = new XMLHttpRequest();
+            xhr.onload = function () {
+              callback(this.responseText)
+            };
+
+
+          /// Saying things over
+          /// multiple lines of code
+
+          /* Multiline comments */
+          /**
+           * Multiline comments 2
+           *
+           */
+
+            xhr.open('GET', /*hi*/url, true);
+            xhr.send();//Sending
+          }//:O
         CODE
 
         expected = <<~CODE
