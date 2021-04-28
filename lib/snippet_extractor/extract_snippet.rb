527ec1ae870d42926b9bf706e11ae49e09ddce6b
Jeremy Walker <jez.walker@gmail.com>
2021-04-28 23:10:34 +0100

    Don't fail on missing language

--- lib/snippet_extractor/extract_snippet.rb
+++ lib/snippet_extractor/extract_snippet.rb
@@ -5,13 +5,20 @@ module SnippetExtractor
     initialize_with :code, :language
 
     def call
+      processed_lines[0...10].join
+    end
+
+    private
+    def processed_lines
       lines.drop_while do |line|
         naked_line = line.strip
         next true if naked_line.empty?
         next true if ignore_list.any? { |ignore| naked_line.start_with?(ignore) }
 
         false
-      end[0...10].join
+      end
+    rescue Errno::ENOENT
+      lines
     end
 
     def lines
