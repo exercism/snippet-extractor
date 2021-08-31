e215b05232d4c26c78c40356583a20f678d668ff
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-08-31 20:37:32 +0200

    Remove trailing whitespace

--- test/languages/elixir_test.rb
+++ test/languages/elixir_test.rb
@@ -408,7 +408,7 @@ module SnippetExtractor
             @doc """
             Two-fer or 2-fer is short for two for one. One for you and one for me.
             """
-            
+
             @spec two_fer(String.t()) :: String.t()
             def two_fer(name \\ "you") when is_binary(name) do
               "One for \#{name}, one for me"
