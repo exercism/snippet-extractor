895c4977e501b75c241f2062669b7381d2755ea7
habere-et-dispertire <github.eggs@neverbox.com>
2022-09-26 22:30:25 +0100

    Create raku_test.rb
    Tests for single and multi-line raku comments.

---
+++ test/languages/raku_test.rb
@@ -0,0 +1,141 @@
+require "test_helper"
+
+module SnippetExtractor
+  module Languages
+    class RakuTest < Minitest::Test
+      def test_single
+        code = <<~CODE
+          # Single line comment, single line
+          sub foo { # Single line comment, partial line
+            say 'baz';
+          }
+        CODE
+
+        expected = <<~CODE
+          sub foo {
+            say 'baz';
+          }
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :raku)
+      end
+
+      def test_multi_round
+        code = <<~CODE
+          #`( Multi-line comment, round parentheses, single line )
+          #`(( Multi-line comment, double round parentheses, single line ))
+          sub foo { #`( Multi-
+            line comment, single round parentheses, multiple lines )  say #`(( Multi-
+            line comment, double round parentheses, multiple lines ))  'baz';
+          }
+        CODE
+
+        expected = <<~CODE
+          sub foo {
+            say
+            'baz';
+          }
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :raku)
+      end
+
+      def test_multi_code
+        code = <<~CODE
+          #`{ Multi-line comment, code parentheses, single line }
+          #`{{ Multi-line comment, double code parentheses, single line }}
+          sub foo { #`{ Multi-
+            line comment, code parentheses, multiple lines }  say #`{{ Multi-
+            line comment, double code parentheses, multiple lines }}  'baz';
+          }
+        CODE
+
+        expected = <<~CODE
+          sub foo {
+            say
+            'baz';
+          }
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :raku)
+      end
+
+      def test_multi_square
+        code = <<~CODE
+          #`[ Multi-line comment, square parentheses, single line ]
+          #`[[ Multi-line comment, double square parentheses, single line ]]
+          sub foo { #`[ Multi-
+            line comment, square parentheses, multiple lines ]  say #`[[ Multi-
+            line comment, double square parentheses, multiple lines ]]  'baz';
+          }
+        CODE
+
+        expected = <<~CODE
+          sub foo {
+            say
+            'baz';
+          }
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :raku)
+      end
+
+      def test_multi_angle
+        code = <<~CODE
+          #`< Multi-line comment, angle parentheses, single line >
+          #`<< Multi-line comment, double angle parentheses, single line >>
+          sub foo { #`< Multi-
+            line comment, angle parentheses, multiple lines >  say #`<< Multi-
+            line comment, double angle parentheses, multiple lines >>  'baz';
+          }
+        CODE
+
+        expected = <<~CODE
+          sub foo {
+            say
+            'baz';
+          }
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :raku)
+      end
+
+      def test_pod
+        code = <<~CODE
+          =begin comment
+            Hello,
+            World!
+          =end comment
+          sub foo {
+            say 'baz';
+          }
+        CODE
+
+        expected = <<~CODE
+          sub foo {
+            say 'baz';
+          }
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :raku)
+      end
+
+      def test_unit_module
+        code = <<~CODE
+          unit module HelloWorld;
+          sub foo {
+            say 'baz';
+          }
+        CODE
+
+        expected = <<~CODE
+          sub foo {
+            say 'baz';
+          }
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :raku)
+      end
+    end
+  end
+end
