faa529fc5767edea013a46030b2aa4af0b337c1b
Mike Pokraka <mike@quirky.me.uk>
2022-09-28 07:55:44 +0100

    Customize ABAP snippet extractor (#42)
    Customize ABAP snippet extractor
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

---
+++ test/languages/abap_test.rb
@@ -0,0 +1,53 @@
+require "test_helper"
+
+module SnippetExtractor
+  module Languages
+    class AbapTest < Minitest::Test
+      def test_simple_example
+        code = <<~CODE
+          method run.
+          * delete me
+            write 'Hello World!'.
+            "another comment
+          endmethod.
+        CODE
+
+        expected = <<~CODE
+          method run.
+            write 'Hello World!'.
+          endmethod.
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :abap)
+      end
+
+      def test_strip_definition
+        code = <<~CODE
+          class foo definition.
+            public section.
+              methods run.
+            private section.
+              data stuff type standard table of string.
+          endclass.
+
+          class foo implementation.
+            method run.
+          * comment1
+              write 'Hello world!'.
+              "comment2
+            endmethod.
+          endclass.
+        CODE
+
+        expected = <<~CODE
+            method run.
+              write 'Hello world!'.
+            endmethod.
+          endclass.
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :abap)
+      end
+    end
+  end
+end
