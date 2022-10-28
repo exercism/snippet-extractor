635fd1bf28f2e8aee9609fc62318fd89ee7dc519
Daniel Mita <966706+m-dango@users.noreply.github.com>
2022-10-28 12:48:00 +0100

    Add basic mode checks for perl (#57)
    Add basic mode checks for perl
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

---
+++ test/languages/perl5_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+module SnippetExtractor
+  module Languages
+    class PerlTest < Minitest::Test
+      def test_all
+        code = <<~CODE
+          package HelloWorld;
+          use v5.36;
+          use 5.036;
+          use strict;
+          use warnings;
+          use feature qw<say>;
+          use Exporter qw<import>;
+          our @EXPORT_OK = qw<hello>;
+
+          # test comment
+          sub hello {
+              return 'Hello, World!';
+          }
+        CODE
+        expected = <<~CODE
+          sub hello {
+              return 'Hello, World!';
+          }
+        CODE
+        assert_equal expected, ExtractSnippet.(code, 'perl5')
+      end
+    end
+  end
+end
