4dea084977625febbcc019e2798c8a35fe9ab0cf
loziniak <maciej@robotix-lozinski.pl>
2022-11-28 10:07:32 +0100

    add Red language

---
+++ test/languages/red_test.rb
@@ -0,0 +1,43 @@
+require "test_helper"
+
+module SnippetExtractor
+  module Languages
+    class RedTest < Minitest::Test
+      def test_full_example
+        code = <<~CODE
+          Red [] ; a comment
+
+          ; single line comment
+          x: 1 ; line comment 1
+          x: 2   ;-- line comment 2
+          x: 3		;@@ line comment 3
+
+          comment ['this
+          	'is 'multiline
+          	'comment]
+          comment   {and this
+          	as well}
+
+          function add100 [x [integer!]] [
+          	"; this should not count as comment "
+          	{ comment [and neither
+          	 this] }
+          ]
+        CODE
+
+        expected = <<~CODE
+          x: 1
+          x: 2
+          x: 3
+          function add100 [x [integer!]] [
+          	"; this should not count as comment "
+          	{ comment [and neither
+          	 this] }
+          ]
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :red)
+      end
+    end
+  end
+end
