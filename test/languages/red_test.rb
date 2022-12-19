0c374d935c1e75095d76883f8b3d2aee1dbb686d
loziniak <maciej@robotix-lozinski.pl>
2022-12-19 10:19:19 +0100

    Red: extractor will clear trailing whitespace
    references #65

--- test/languages/red_test.rb
+++ test/languages/red_test.rb
@@ -30,10 +30,10 @@ module SnippetExtractor
         CODE
 
         expected = <<~CODE
-          x: 1 
+          x: 1
           y: 11
-          x: 2   
-          x: 3		
+          x: 2
+          x: 3
 
           print "no comments!"
 
