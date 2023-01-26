d8a00db546d99b77b497a57e952205ea5548988b
meatball <69751659+meatball133@users.noreply.github.com>
2023-01-26 21:15:25 +0100

    added tests

---
+++ test/languages/crystal_test.rb
@@ -0,0 +1,61 @@
+require "test_helper"
+
+module SnippetExtractor
+  module Languages
+    class RubyTest < Minitest::Test
+      def test_full_example
+        code = <<~CODE
+          # This is a file
+          # With some comments in it
+
+          # And a blank line ⬆️
+          # It has some requires like this:
+          require 'json'
+
+          # And then eventually the code
+          class TwoFer
+            ...
+          end
+        CODE
+
+        expected = <<~CODE
+          class TwoFer
+            ...
+          end
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :ruby)
+      end
+
+      def test_extended_example
+        code = <<~CODE
+          # This is a file
+          # With some comments in it
+
+          # And a blank line ⬆️
+          # It has some requires like this:
+          require 'json'
+          # And then eventually the code
+          class TwoFer
+            ...#And comments
+          end
+          __END__
+          More data after end of source file
+          and more
+          moooore but this is over 10 lines so it wont appear
+        CODE
+
+        expected = <<~CODE
+          class TwoFer
+            ...#And comments
+          end
+          __END__
+          More data after end of source file
+          and more
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :ruby)
+      end
+    end
+  end
+end
