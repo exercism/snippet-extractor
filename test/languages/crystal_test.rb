dc20b4f3a22ed1b23999df5edd34572dbadaa7d9
meatball <69751659+meatball133@users.noreply.github.com>
2023-01-27 08:58:14 +0100

    [New language]: Crystal (#69)
    * add support for crystal
    
    * added tests
    
    * Updated tests
    
    * updated test
    
    * added missing line
    
    * fixed intdentation
    
    * Update test/languages/crystal_test.rb
    
    * Update test/languages/crystal_test.rb
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

---
+++ test/languages/crystal_test.rb
@@ -0,0 +1,89 @@
+require "test_helper"
+
+module SnippetExtractor
+  module Languages
+    class CrystalTest < Minitest::Test
+      def test_full_example
+        code = <<~CODE
+          # This is a file
+          # With some comments in it
+
+          # And a blank line ⬆️
+          # It has some requires like this:
+          require 'json'
+
+          # And then eventually the code
+          class TwoFer
+            ...
+          end
+        CODE
+
+        expected = <<~CODE
+          class TwoFer
+            ...
+          end
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :crystal)
+      end
+
+      def test_extended_example
+        code = <<~CODE
+          # This is a file
+          # With some comments in it
+
+          # And a blank line ⬆️
+          # It has some requires like this:
+          require 'json'
+          # And then eventually the code
+          class TwoFer
+            ...#And comments
+          end
+        CODE
+
+        expected = <<~CODE
+          class TwoFer
+            ...
+          end
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :crystal)
+      end
+
+      def test_full_example_extended
+        code = <<~CODE
+          #!/usr/bin/env python3
+
+          # The above is sometimes used to note
+          # where the python interpreter is located.
+
+          # And after this comes the import statements, if any:
+          require "json"
+
+          # And then any constants or globals,
+          # followed by code.
+          WORDS = ["abc", def]
+
+          module
+            def count_words(text)
+              #This counts words
+              WORDS[0]
+            end
+          end
+        CODE
+
+        expected = <<~CODE
+          WORDS = ["abc", def]
+
+          module
+            def count_words(text)
+              WORDS[0]
+            end
+          end
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :crystal)
+      end
+    end
+  end
+end
