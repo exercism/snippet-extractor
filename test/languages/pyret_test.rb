eeee4e275b61f49ac60ce818d3a03a30e030938a
Andras Nagy <20251272+BNAndras@users.noreply.github.com>
2023-07-27 16:55:49 -0700

    Indentation fix

--- test/languages/pyret_test.rb
+++ test/languages/pyret_test.rb
@@ -5,25 +5,25 @@ module SnippetExtractor
     class PyretTest < Minitest::Test
       def test_full_example
         code = <<~CODE
-        provide: foo end
+          provide: foo end
 
-        include lists
-        import string-dict as SD
+          include lists
+          import string-dict as SD
 
-        # Standalone single line comment
-        #|
-            This comment can extend
-            over multiple lines
-        |#
-        fun foo(text):
-          true # Hi I start after some code
-        end
+          # Standalone single line comment
+          #|
+              This comment can extend
+              over multiple lines
+          |#
+          fun foo(text):
+            true # Hi I start after some code
+          end
         CODE
 
         expected = <<~CODE
-        fun foo(text):
-          true 
-        end
+          fun foo(text):
+            true 
+          end
         CODE
 
         assert_equal expected, ExtractSnippet.(code, :pyret)
