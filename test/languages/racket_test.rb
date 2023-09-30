f3c1259e78ab8edf959732e92a0bd9d66bd31cd0
vaeng <34183939+vaeng@users.noreply.github.com>
2023-09-30 11:21:27 +0200

    fix(racket): remove multiline matching Many solutions show up empty. This commit will restablish those, but multline require and provides will be cut now. This is way better than empty solutions though.

--- test/languages/racket_test.rb
+++ test/languages/racket_test.rb
@@ -13,9 +13,9 @@ module SnippetExtractor
           (provide add-gigasecond)
           ; only comment in a line
           (require racket/date)
-          (provide (contract-out
-            [add-gigasecond (-> date? date?)]))
-          ; ^ Multiline provide
+          ;(provide (contract-out
+          ;  [add-gigasecond (-> date? date?)]))
+          ; ^ Multiline provide no solution yet to conquer that 
           (define gigasecond 1000000000)
 
           (define (add-gigasecond dt) ; comment after important stuff
@@ -29,6 +29,45 @@ module SnippetExtractor
           (define (add-gigasecond dt) 
             (seconds->date
               (+ (date->seconds dt) gigasecond)))
+        CODE
+
+        assert_equal expected, ExtractSnippet.(code, :racket)
+      end
+
+      def test_many_braces_example
+        code = <<~CODE
+          #lang racket
+
+          (provide score)
+          
+          (define score (scrabble-score
+                        (1 A E I O U L N R S T)
+                        (2 D G)
+                        (8 J X)
+                        (10 Q Z)))
+          (define-syntax (scrabble-score stx)
+            (syntax-case stx ()
+              [(_ (num char ...) ...)
+              #'(lambda (word)
+                  (apply + (map (lambda (s)
+                                  (cond
+                                    [(member (string-upcase (string s))
+                                              (list (symbol->string (quote char)) ...)) num] ...
+                                    [else 0]))
+                                (string->list word))))]))
+        CODE
+
+        expected = <<~CODE         
+          (define score (scrabble-score
+                        (1 A E I O U L N R S T)
+                        (2 D G)
+                        (8 J X)
+                        (10 Q Z)))
+          (define-syntax (scrabble-score stx)
+            (syntax-case stx ()
+              [(_ (num char ...) ...)
+              #'(lambda (word)
+                  (apply + (map (lambda (s)
         CODE
 
         assert_equal expected, ExtractSnippet.(code, :racket)
