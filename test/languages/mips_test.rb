7ffa9d41c790a600d2aa2684c2f058037b3cf1bf
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-09 09:01:51 +0100

    Tweak MIPS

--- test/languages/mips_test.rb
+++ test/languages/mips_test.rb
@@ -6,29 +6,31 @@ class SnippetExtractor::Languages::MipsTest < Minitest::Test
       # General comment
       #
       # $a0 - input
-      
+
       .globl binary_convert
-      
+
+      .data
+
+      .text
+
       binary_convert:
               li      $v0, 0 # line comment
-      
+
       # Another line comment
       end:
               jr $ra
-        
+
     CODE
 
-    expected = <<~CODE          
-      .globl binary_convert
-      
+    expected = <<~CODE
       binary_convert:
               li      $v0, 0 
 
       end:
               jr $ra
-      
+
     CODE
 
-    assert_equal expected, SnippetExtractor::ExtractSnippet.(code, :nim)
+    assert_equal expected, SnippetExtractor::ExtractSnippet.(code, :mips)
   end
 end
