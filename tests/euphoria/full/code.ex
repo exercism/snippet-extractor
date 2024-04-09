8c478b9f173abf4741f981c12bbbb3d58b583e30
Glenn Jackman <glenn.jackman@gmail.com>
2024-04-09 13:36:34 -0400

    add euphoria

---
+++ tests/euphoria/full/code.ex
@@ -0,0 +1,14 @@
+/* This is a file
+ * With some comments in it
+ */
+
+include std/sequence.e
+
+-- And a blank line ⬆️
+
+-- And then eventually the code
+public function afunc()
+    return 42
+end function
+
+afunc()
