5a60a6af0f8437d78fa976aa6b59b20e5cb1ff18
Murat Kirazkaya <groophylifefor@gmail.com>
2024-08-21 23:59:11 +0300

    git detection failure fix

--- tests/batch/full/code.bat
+++ tests/batch/full/code.bat
@@ -2,7 +2,6 @@
 :: Disable command echoing
 
 SET source_dir=C:\Users\Username\Documents
-SET backup_dir=D:\Backup
 
 REM Notify start of backup
 echo Starting backup...
@@ -12,15 +11,3 @@ IF NOT EXIST "%source_dir%" (
     echo Source not found. Exiting...
     exit /b
 )
-
-IF NOT EXIST "%backup_dir%" (
-    REM Create backup directory if it does not exist
-    echo Creating backup directory...
-    mkdir "%backup_dir%"
-)
-
-xcopy "%source_dir%\*" "%backup_dir%\" /E /I /Y
-:: Perform backup, including subdirectories
-
-echo Backup complete.
-pause
