c199fdb04cd671f3c8121c4ec19628c01647c6c4
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-07-25 00:16:14 -0700

    add vbnet support (#143)

---
+++ tests/vbnet/full/code.vb
@@ -0,0 +1,30 @@
+Imports System
+
+Public Module Grains
+    ''' <summary>
+    ''' Calculate the number of grains on a square.
+    ''' </summary>
+    ''' <paramname="n"></param>
+    ''' <returns></returns>
+    Public Function Square(ByVal n As Integer) As ULong
+        If n <= 0 OrElse n > 64 Then
+            Throw New ArgumentOutOfRangeException(NameOf(n))
+        End If
+
+        Return If(n = 1, 1, 2 * Square(n - 1))
+    End Function
+
+    ''' <summary>
+    ''' Calculate the total number of grains on the chessboard.
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function Total() As ULong
+        Dim lTotal As ULong = 0
+
+        For i = 1 To 64
+            lTotal += Square(i)
+        Next
+
+        Return lTotal
+    End Function
+End Module
